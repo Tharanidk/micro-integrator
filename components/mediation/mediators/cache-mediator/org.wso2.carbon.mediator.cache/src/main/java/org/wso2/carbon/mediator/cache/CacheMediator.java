559e9abca4986ccf84bfa8a7e26b0397b1373e9a
dulith <dulithcsenanayake@gmail.com>
2022-08-23 08:32:40 +0530

    Clone Transport Header Map

--- components/mediation/mediators/cache-mediator/org.wso2.carbon.mediator.cache/src/main/java/org/wso2/carbon/mediator/cache/CacheMediator.java
+++ components/mediation/mediators/cache-mediator/org.wso2.carbon.mediator.cache/src/main/java/org/wso2/carbon/mediator/cache/CacheMediator.java
@@ -50,6 +50,7 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
@@ -371,11 +372,12 @@ public class CacheMediator extends AbstractMediator implements ManagedLifecycle,
             msgCtx.removeProperty(Constants.Configuration.CONTENT_TYPE);
         }
         if ((headerProperties = cachedResponse.getHeaderProperties()) != null) {
-
+            Map clonedMap = new HashMap();
+            clonedMap.putAll(headerProperties);
             msgCtx.setProperty(org.apache.axis2.context.MessageContext.TRANSPORT_HEADERS,
-                    headerProperties);
+                    clonedMap);
             msgCtx.setProperty(Constants.Configuration.MESSAGE_TYPE,
-                    headerProperties.get(Constants.Configuration.MESSAGE_TYPE));
+                    clonedMap.get(Constants.Configuration.MESSAGE_TYPE));
         }
 
         // take specified action on cache hit
